a8c015b2cf4725e5b4770000ea0d1c15ef43c078
JulioJosueG <jh2019-0417@uce.edu.do>
2021-06-29 23:11:48 -0400

    Add project files.

---
+++ CRUDRazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDRazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IDbConnection _connection;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(
+            IDbConnection connection,
+            ILogger<IndexModel> logger)
+        {
+            _connection = connection;
+            _logger = logger;
+        }
+
+        public List<Customers> Customers { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
